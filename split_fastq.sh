4e45843a58584844432b8f3b5075510c5947ec4a
mjpdejong <mail@mjpdejong.com>
2018-11-08 18:40:34 +0100

    2nd commit

---
+++ split_fastq.sh
@@ -0,0 +1,17 @@
+reads=test.fastq
+outputfolder=$PWD
+round=1
+
+
+cat -n $reads | sed -n 1~4p | awk '{print $2 " " $1 " " $1+1 " " $1+2 " " $1+3}' | tr -d '@' | sort > $outputfolder/fastqindex.txt
+
+cat -n $reads | sort -k 1b,1 > $outputfolder/fastq.sorted
+
+for paf in $outputfolder/racon-$round/*.paf ; do
+
+    bin=$(basename $paf | cut -d . -f 1)
+
+    join $outputfolder/fastq.sorted <( join <( cat $outputfolder/fastqindex.txt ) <(awk '{print $1}' $paf | sort -u ) | awk '{print $2 "\n" $3 "\n" $4 "\n" $5}' | sort ) | sort -n | cut -d ' ' -f 2- > $outputfolder/racon-$round/$bin.fastq
+
+done
+
