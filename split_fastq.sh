04b4277ade3c0c0b64191d1d344bc95092098a24
mjpdejong <mail@mjpdejong.com>
2018-11-08 19:10:03 +0100

    update

--- split_fastq.sh
+++ split_fastq.sh
@@ -11,7 +11,15 @@ for paf in $outputfolder/round-$round/*.paf ; do
 
     bin=$(basename $paf | cut -d . -f 1)
 
-    join $outputfolder/fastq.sorted <( join <( cat $outputfolder/fastqindex.txt ) <(awk '{print $1}' $paf | sort -u ) | awk '{print $2 "\n" $3 "\n" $4 "\n" $5}' | sort ) | sort -n | cut -d ' ' -f 2- > $outputfolder/round-$round/$bin.fastq
+    join $outputfolder/fastq.sorted <( \
+
+		join <( cat $outputfolder/fastqindex.txt ) <( awk '{print $1}' $paf | sort -u ) | \
+
+		awk '{print $2 "\n" $3 "\n" $4 "\n" $5}' | sort \
+
+	) | \
+
+	sort -n | cut -d ' ' -f 2- > $outputfolder/round-$round/$bin.fastq
 
 done
 
